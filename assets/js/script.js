f72b706814f8be6c203f28a3944d7abc7b835077
Engelina-Goncalves <engelina.grilo@gmail.com>
2025-02-05 20:21:03 +0100

    created skills.html content and edited stylesheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,50 @@
-console.log("Welcome to my portfolio!");
+const skills = {
+    programming: ["Python", "Powershell", "HTML & CSS"],
+    languages: ["English", "German", "French", "Luxembourgish", "Portuguese", "Spanish"],
+    tools: ["Git", "VS Code", "Linux"],
+    softskills: ["Technical Writing", "Public Speaking", "Communication"]
+};
+
+let currentIndex = {
+    programming: 0,
+    languages: 0,
+    tools: 0,
+    softskills: 0
+};
+
+let currentCategory = 'programming';
+
+function cycleSkills() {
+    const categorySkills = skills[currentCategory];
+    const skillElement = document.getElementById(`${currentCategory}-skill`);
+    skillElement.style.opacity = 0;
+    setTimeout(() => {
+        skillElement.innerText =categorySkills[currentIndex[currentCategory]];
+        skillElement.style.opacity = 1;
+        currentIndex[currentCategory]++;
+        if (currentIndex[currentCategory] >= categorySkills.length) {
+            currentIndex[currentCategory] = 0
+            moveToNextCategory();
+        }
+    }, 1000);
+}
+
+function moveToNextCategory(){
+    switch (currentCategory) {
+        case 'programming':
+            currentCategory = 'languages';
+            break
+        case 'languages':
+            currentCategory = 'tools';
+            break
+        case 'tools':
+            currentCategory = 'softskills';
+            break
+        case 'softskills':
+            currentCategory = 'programming';
+            return;
+    }
+    setTimeout(cycleSkills, 3000);
+}
+
+cycleSkills();
